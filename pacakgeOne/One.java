a4778a16cba1dc02bd2c7fca1b77bc3f15b24931
Nikhil Bagde <nikhil.bagde@tn.gov>
2023-11-09 15:49:36 -0600

    TN-12345 Resetting

--- pacakgeOne/One.java
+++ pacakgeOne/One.java
@@ -6,19 +6,5 @@ public class One {
         System.out.println("Second Commit");
         System.out.println("Third Commit");
 
-        System.out.println("Feature First Commit");
-        System.out.println("Feature Second Commit");
-        System.out.println("Feature Third Commit");
-
-        System.out.println("Forth Commit");
-        System.out.println("Fifth Commit");
-        System.out.println("Sixth Commit");
-
-        System.out.println("Feature-CR-7.0 First Commit");
-        System.out.println("Feature-CR-7.0 Second Commit");
-        System.out.println("Feature-CR-7.0 Third Commit");
-
-
-
     }
 }
