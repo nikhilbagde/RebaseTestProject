d4c6bd455b9d65712810cfe49ed7b39c5da182d3
Nikhil Bagde <nikhil.bagde@tn.gov>
2023-11-09 15:48:12 -0600

    TN-12345 Feature-2.1 First

--- pacakgeOne/One.java
+++ pacakgeOne/One.java
@@ -2,23 +2,9 @@ package pacakgeOne;
 
 public class One {
     public static void main(String[] args) {
-        System.out.println("First Commit");
+        System.out.println("First Commit 2.1");
         System.out.println("Second Commit");
         System.out.println("Third Commit");
-
-        System.out.println("Feature First Commit");
-        System.out.println("Feature Second Commit");
-        System.out.println("Feature Third Commit");
-
-        System.out.println("Forth Commit");
-        System.out.println("Fifth Commit");
-        System.out.println("Sixth Commit");
-
-        System.out.println("Feature-CR-7.0 First Commit");
-        System.out.println("Feature-CR-7.0 Second Commit");
-        System.out.println("Feature-CR-7.0 Third Commit");
-
-
 
     }
 }
